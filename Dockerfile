abe5cbd041ab3fe5ea97dbbdaf5ae702163e94b9
satriam <satriabae43@gmail.com>
2023-09-27 11:29:14 +0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM princemendiratta/botsapp:latest
+
+WORKDIR /
+
+COPY . /BotsApp
+
+WORKDIR /BotsApp
+
+RUN git init --initial-branch=multi-device
+
+RUN git remote add origin https://github.com/Prince-Mendiratta/BotsApp.git
+
+RUN git fetch origin multi-device
+
+RUN git reset --hard origin/multi-device
+
+RUN yarn
+
+# RUN cp -r /root/Baileys/lib /BotsApp/node_modules/@whiskeysockets/baileys/
+
+CMD [ "npm", "start"]
---
+++ database/auth.ts
@@ -0,0 +1,32 @@
+import config from "../config";
+import { DataTypes, InferAttributes, Model, InferCreationAttributes, Sequelize } from "sequelize";
+
+const sequelize: Sequelize = config.DATABASE;
+
+// @ts-ignore
+class Auth extends Model<InferAttributes<Auth>, InferCreationAttributes<Auth>> {
+    declare key: string;
+    declare value: string;
+    declare type: string;
+}
+
+Auth.init({
+    // @ts-ignore
+        key: {
+            type: DataTypes.STRING(1000000),
+            allowNull: false,
+        },
+        value: {
+            type: DataTypes.STRING(1000000)
+        },
+        type: {
+            type: DataTypes.STRING(1000000),
+        }
+    }, {
+        sequelize,
+        tableName: "Authentication",
+        timestamps: false,
+    }
+);
+
+export {Auth};
